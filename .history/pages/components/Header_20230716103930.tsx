d1029a5ab96722f1751869657b199de125dd86e0
cozocozy <cozocozy@gmail.com>
2023-07-16 22:01:01 +0700

    desktop version newer

---
+++ .history/pages/components/Header_20230716103930.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { BsArrowDownSquare, BsSearch } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <div className="fixed top-0 z-50 bg-black w-full">
+      <div className="flex justify-between items-center py-1 ">
+        <div className="flex ml-4 mx-3 lg:hidden">
+          <AiOutlineMenu size={20} className="mt-2" />
+          <h3 className="text-2xl text-green-500 font-bold ml-2 ">IQIYI</h3>
+        </div>
+        <div className="flex justify-center items-center w-full mt-1 ">
+          {/* <button className="absolute border-l border-gray-500 ml-60 p-1 px-4">
+            <BsSearch />
+          </button> */}
+          <input className="rounded rounded-md bg-neutral-700 w-full max-w-2xl pl-3 pb-1" placeholder="search here.." autoComplete="off"></input>
+        </div>
+        <button className="flex flex-row mr-7 mt-1 rounded rounded-md w-24 justify-evenly mx-3 bg-green-500 text-gray-100 items-center">
+          <div className="">
+            <BsArrowDownSquare size={15} />
+          </div>
+          <p className="text-sm">APP</p>
+        </button>
+      </div>
+      <div className="flex flex-row  p-3 overflow-x-auto container-snap whitespace-nowrap space-x-6 ml-3 text-gray-300">
+        <Link href="#" className="text-white pages">
+          For You
+        </Link>
+        <Link href="#" className=" pages">
+          Destined
+        </Link>
+        <Link href="#" className=" pages">
+          Drama
+        </Link>
+        <Link href="#" className=" pages">
+          K-Drama
+        </Link>
+        <Link href="#" className=" pages">
+          Movies
+        </Link>
+        <Link href="#" className=" pages">
+          Anime
+        </Link>
+        <Link href="#" className=" pages">
+          Variety Show
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ .history/pages/components/Header_20230716105440.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { BsArrowDownSquare, BsSearch } from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <div className="fixed top-0 z-50 bg-black w-full">
+      <div className="flex justify-between items-center py-1 ">
+        {/* left part */}
+        <div className="flex ml-4 mx-3 lg:hidden">
+          <AiOutlineMenu size={20} className="mt-2" />
+          <h3 className="text-2xl text-green-500 font-bold ml-2 ">IQIYI</h3>
+        </div>
+
+        {/* middle part */}
+        <div className="flex justify-center items-center w-full mt-1 rounded bg-neutral-700 ">
+          <input className=" w-full max-w-2xl pl-3 pb-1" placeholder="search here.." autoComplete="off"></input>
+          <button className="">
+            <BsSearch />
+          </button>
+        </div>
+
+        {/* right part */}
+        <button className="flex flex-row mr-8 mt-1 rounded rounded-md h-7 justify-center mx-3 bg-green-500 text-gray-100 items-center">
+          <div className="mx-2 mr-1">
+            <BsArrowDownSquare size={14} />
+          </div>
+          <p className="text-sm mr-2">APP</p>
+        </button>
+      </div>
+      <div className="flex flex-row  p-3 overflow-x-auto container-snap whitespace-nowrap space-x-6 ml-3 text-gray-300">
+        <Link href="#" className="text-white pages">
+          For You
+        </Link>
+        <Link href="#" className=" pages">
+          Destined
+        </Link>
+        <Link href="#" className=" pages">
+          Drama
+        </Link>
+        <Link href="#" className=" pages">
+          K-Drama
+        </Link>
+        <Link href="#" className=" pages">
+          Movies
+        </Link>
+        <Link href="#" className=" pages">
+          Anime
+        </Link>
+        <Link href="#" className=" pages">
+          Variety Show
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
